3440ac6dac047f9e63eb460e5a3300f30fba5c6c
AxAkumax <akshita.akumalla@gmail.com>
2024-11-10 13:46:43 -0800

    removed all crawler files and added indexer.py

---
+++ indexer.py
@@ -0,0 +1,13 @@
+from parser import convert_response_to_words, compute_word_frequencies
+#import nltk porter stemming
+
+def convert_freq_stemming(response_content):
+    pass
+
+
+def indexer:
+    pass
+
+
+def run:
+    pass
