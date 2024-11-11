6d645c7172801f0ea247ce1ef842eda10bb7e61f
tnguyenner <tyler.h.nguyen7@gmail.com>
2024-11-11 12:25:38 -0800

    stem freq.

--- indexer.py
+++ indexer.py
@@ -1,7 +1,18 @@
 from parser import convert_response_to_words, compute_word_frequencies
-#import nltk porter stemming
+from nltk.stem import PorterStemmer
+import nltk
 
+stemmer = PorterStemmer()
 def convert_freq_stemming(response_content):
+    try:
+        text = convert_response_to_words(response_content)
+        #stopwords
+        stem_word = [stemmer.stem(word) for word in text]
+        frequency = compute_word_frequencies(stem_word)
+    except Exception as e:
+        print(f"Error processing stemmed token frequency: {e}")
+        return {}
+    return frequency
     pass
 
 
